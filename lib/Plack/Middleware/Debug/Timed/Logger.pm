fbe3e74fe48b3cfc6c3044e6db877dc192b0eb6a
Nikolay Martynov <nmartynov@shutterstock.com>
2013-09-18 18:46:58 -0400

    Up version for cpan upload

--- lib/Plack/Middleware/Debug/Timed/Logger.pm
+++ lib/Plack/Middleware/Debug/Timed/Logger.pm
@@ -15,11 +15,11 @@ Plack::Middleware::Debug::Timed::Logger - An Event Log Debug Panel
 
 =head1 VERSION
 
-Version 0.0.2
+Version 0.0.3
 
 =cut
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 
 =head1 SYNOPSIS
 
